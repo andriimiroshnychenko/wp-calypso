4101dfdfe6417d37ca3b832965204dbe3483707d
Kirk Wight <kwight@kwight.ca>
2020-04-15 16:06:41 -0700

    Remove redundant "Card" part of component name.

--- client/my-sites/customer-home/locations/secondary/index.jsx
+++ client/my-sites/customer-home/locations/secondary/index.jsx
@@ -9,7 +9,7 @@ import React from 'react';
 import FreePhotoLibrary from 'my-sites/customer-home/cards/education/free-photo-library';
 import GoMobile from 'my-sites/customer-home/cards/features/go-mobile';
 import GrowEarn from 'my-sites/customer-home/cards/features/grow-earn';
-import QuickStartCard from 'my-sites/customer-home/cards/features/quick-start';
+import QuickStart from 'my-sites/customer-home/cards/features/quick-start';
 import LaunchSite from 'my-sites/customer-home/cards/features/launch-site';
 import Stats from 'my-sites/customer-home/cards/features/stats';
 import Support from 'my-sites/customer-home/cards/features/support';
